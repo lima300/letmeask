a8a60865bcd294c91746349ab260c3cc0b0fb3bd
Otávio Lima <olima94@gmail.com>
2021-06-23 21:31:17 -0300

    :rocket: criar sala

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,19 +1,45 @@
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { FormEvent } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 import illustrationImg from '../assests/images/illustration.svg';
 import logo from '../assests/images/logo.svg';
 
 import { Button } from '../components/Button';
+import { database } from '../services/firebase';
 
 import '../styles/auth.scss';
-// import { useAuth } from "../hooks/useAuth";
+import { useAuth } from "../hooks/useAuth";
 
 export function NewRoom() {
-  // const { user } = useAuth();
+  const { user } = useAuth();
+  const history = useHistory();
+  const [newRoom,setNewRoom] = useState("");
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if(newRoom.trim() === '') {
+      return;
+    }
+
+    const roomRef = database.ref('rooms');
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    });
+
+    history.push(`/rooms/${firebaseRoom.key}`);
+  }
+
   return (
     <div id="page-auth">
       <aside>
-        <img src={illustrationImg} alt="Ilustrção simbolizando perguntas e respostas" />
+        <img
+          src={illustrationImg}
+          alt="Ilustrção simbolizando perguntas e respostas"
+        />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidadas da sua audiência em tempo-real</p>
       </aside>
@@ -21,14 +47,14 @@ export function NewRoom() {
         <div className="main-content">
           <img src={logo} alt="Letmeask" />
           <h2>Criar uma nova sala</h2>
-          <form>
+          <form onSubmit={handleCreateRoom}>
             <input 
-              type="text"
-              placeholder="Nome da sala"    
+              type="text" 
+              placeholder="Nome da sala"
+              onChange={event => setNewRoom(event.target.value)}
+              value={newRoom}
             />
-            <Button type="submit" >
-              Criar sala
-            </Button>
+            <Button type="submit">Criar sala</Button>
           </form>
           <p>
             Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
@@ -37,5 +63,4 @@ export function NewRoom() {
       </main>
     </div>
   );
-
 }
---
+++ src/pages/Room.tsx
@@ -0,0 +1,5 @@
+export function Room() {
+  return (
+    <h1>fala galera</h1>
+  )
+}
